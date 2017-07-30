f1a045c71424719eebfd6dd5ee98cc48f203c67f
Rajus Dhoble <radhoble@microsoft.com>
2017-07-29 23:29:53 -0700

    feat: Created BookShelf component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as BooksAPI from './BooksAPI';
+import BookShelf from './BookShelf';
 
-class App extends Component {
+class BooksApp extends Component {
+  state = {
+    /*
+      TODO: Maintain state of books here.
+
+    */
+    books : []
+
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {      
+      this.setState({ books: books });
+    });
+  }
+
+updateBook = (book,newShelf) => {
+  console.log("Updated Book: " + book.title + newShelf);
+}
+
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="app">
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>My Reads</h1>
+          </div>
+          <div className="list-books-content">
+            <BookShelf 
+                books={this.state.books.filter((book)=> book.shelf === 'currentlyReading')}
+                shelfTitle="Currently Reading"
+                onUpdateBook={this.updateBook}
+            />
+            <BookShelf 
+                books={this.state.books.filter((book) => book.shelf === 'wantToRead')}
+                shelfTitle="Want to Read"
+                onUpdateBook={this.updateBook}
+            />
+            <BookShelf 
+                books={this.state.books.filter((book)=> book.shelf === 'read' )}
+                shelfTitle="Read"
+                onUpdateBook={this.updateBook}
+            />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
 }
 
-export default App;
+export default BooksApp;
